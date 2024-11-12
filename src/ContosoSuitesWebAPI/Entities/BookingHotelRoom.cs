f4b5a9699572f802f8ad54c841479ceb48e81e01
rishabhmonga119 <rishabhmonga119@gmail.com>
2024-11-12 11:24:12 +0000

    initial commit

--- src/ContosoSuitesWebAPI/Entities/BookingHotelRoom.cs
+++
@@ -1,7 +0,0 @@
-namespace ContosoSuitesWebAPI.Entities;
-public class BookingHotelRoom
-{
-    public required int BookingHotelRoomID { get; set; }
-    public required int BookingID { get; set; }
-    public required int HotelRoomID { get; set; }
-}
